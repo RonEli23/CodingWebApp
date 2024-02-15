5010f428d24555383d5fa1226fa5586621c1aedc
RonEli23 <roneli386@gmail.com>
2024-02-15 21:29:50 +0200

    fixing bugs

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,12 +1,9 @@
 import axios from 'axios';
-import Cookies from 'js-cookie';
 const BASE_URL = 'https://codingwebapp-api.onrender.com'; //https://codingwebapp-api.onrender.com http://127.0.0.1:8080
 
 
 
 export default axios.create({
-    baseURL: BASE_URL,
-    headers: { 'Content-Type': 'application/json' },
-    withCredentials: true
+    baseURL: BASE_URL
 });
 
