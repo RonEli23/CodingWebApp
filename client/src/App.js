cfc9ad7eed52fadaabbeea425f664e2e180abd84
RonEli23 <roneli386@gmail.com>
2024-02-16 00:58:39 +0200

    fixing bug

--- client/src/App.js
+++ client/src/App.js
@@ -1,36 +1,39 @@
-import './App.css';
+import "./App.css";
 import { Routes, Route } from "react-router-dom";
-import LobbyPage from './components/LobbyPage';
-import ArrayManipulationCode from './components/ArrayManipulationCode';
-import AsyncCode from './components/AsyncCode';
-import BasicFunctionCode from './components/BasicFuncCode';
-import JSONCode from './components/JSONCode';
-import NoPage from './components/NoPage';
-import { useEffect } from 'react';
-import Cookies from 'js-cookie';
-import { v4 as uuidv4 } from 'uuid'; // Import uuid package for generating unique keys
-
+import LobbyPage from "./components/LobbyPage";
+import ArrayManipulationCode from "./components/ArrayManipulationCode";
+import AsyncCode from "./components/AsyncCode";
+import BasicFunctionCode from "./components/BasicFuncCode";
+import JSONCode from "./components/JSONCode";
+import NoPage from "./components/NoPage";
+import { useEffect } from "react";
+import Cookies from "js-cookie";
+import { v4 as uuidv4 } from "uuid"; // Import uuid package for generating unique keys
 
 function App() {
-
+  //create/get cookie at the first render of the app
   useEffect(() => {
-
-    if(!Cookies.get("uniqueKey")){
+    if (!Cookies.get("uniqueKey")) {
       const newUniqueKey = uuidv4();
-      console.log("Generated unique key:", newUniqueKey);
-      Cookies.set("uniqueKey", JSON.stringify(newUniqueKey));
+
+      // Set expiration date
+      let expirationDate = new Date();
+      expirationDate.setDate(expirationDate.getDate() + 30); // This sets the expiration date 30 days from now
+
+      Cookies.set("uniqueKey", JSON.stringify(newUniqueKey), {
+        expires: expirationDate,
+      }); // Setting cookie with expiration date
     }
-    
   }, []);
 
   return (
     <div className="App">
       <Routes>
-        <Route index element={<LobbyPage />}/>
-        <Route path="/AsyncCode" element={<AsyncCode />}/>
-        <Route path="/ArrayManipulation" element={<ArrayManipulationCode />}/>
-        <Route path="/BasicFunction" element={<BasicFunctionCode />}/>
-        <Route path="/JSONCode" element={<JSONCode />}/>
+        <Route index element={<LobbyPage />} />
+        <Route path="/AsyncCode" element={<AsyncCode />} />
+        <Route path="/ArrayManipulation" element={<ArrayManipulationCode />} />
+        <Route path="/BasicFunction" element={<BasicFunctionCode />} />
+        <Route path="/JSONCode" element={<JSONCode />} />
         <Route path="*" exact element={<NoPage />} />
       </Routes>
     </div>
